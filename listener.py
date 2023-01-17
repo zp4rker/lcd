d7cfdd7fd242eadf3b5823d597fb59c7d0b36244
zp4rker <iam@zp4rker.com>
2023-01-17 16:33:19 +1100

    only listen for presses

--- listener.py
+++ listener.py
@@ -23,51 +23,26 @@ def listen():
     GPIO.setup(KEY3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     while True:
-        if GPIO.input(KEY_UP_PIN):
-            # released
-            print("KEY_UP - Released")
-        else:
-            # pressed
-            print("KEY_UP - Pressed")
-        if GPIO.input(KEY_RIGHT_PIN):
-            # released
-            print("KEY_RIGHT - Released")
-        else:
-            # pressed
-            print("KEY_RIGHT - Pressed")
-        if GPIO.input(KEY_DOWN_PIN):
-            # released
-            print("KEY_DOWN - Released")
-        else:
-            # pressed
-            print("KEY_DOWN - Pressed")
-        if GPIO.input(KEY_LEFT_PIN):
-            # released
-            print("KEY_LEFT - Released")
-        else:
-            # pressed
-            print("KEY_LEFT - Pressed")
-        if GPIO.input(KEY_PRESS_PIN):
-            # released
-            print("KEY_PRESS - Released")
-        else:
-            # pressed
-            print("KEY_PRESS - Pressed")
-        if GPIO.input(KEY1_PIN):
-            # released
-            print("KEY1 - Released")
-        else:
-            # pressed
-            print("KEY1 - Pressed")
-        if GPIO.input(KEY2_PIN):
-            # released
-            print("KEY2 - Released")
-        else:
-            # pressed
-            print("KEY2 - Pressed")
-        if GPIO.input(KEY3_PIN):
-            # released
-            print("KEY3 - Released")
-        else:
-            # pressed
-            print("KEY3 - Pressed")
+        if not GPIO.input(KEY_UP_PIN):
+            print("KEY_UP")
+
+        if not GPIO.input(KEY_RIGHT_PIN):
+            print("KEY_RIGHT")
+
+        if not GPIO.input(KEY_DOWN_PIN):
+            print("KEY_DOWN")
+
+        if not GPIO.input(KEY_LEFT_PIN):
+            print("KEY_LEFT")
+
+        if not GPIO.input(KEY_PRESS_PIN):
+            print("KEY_PRESS")
+
+        if not GPIO.input(KEY1_PIN):
+            print("KEY1")
+
+        if not GPIO.input(KEY2_PIN):
+            print("KEY2")
+
+        if not GPIO.input(KEY3_PIN):
+            print("KEY3")
