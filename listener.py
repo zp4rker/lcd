0da46be262a4def8bceb059c0268520208106d18
zp4rker <iam@zp4rker.com>
2023-01-17 16:53:24 +1100

    how about this?

--- listener.py
+++ listener.py
@@ -13,8 +13,6 @@ KEY1_PIN = 21
 KEY2_PIN = 20
 KEY3_PIN = 16
 
-last_press = None
-
 
 def listen():
     GPIO.setmode(GPIO.BCM)
@@ -27,45 +25,43 @@ def listen():
     GPIO.setup(KEY2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(KEY3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    global last_press
-
     while True:
         if not GPIO.input(KEY_UP_PIN):
             print("KEY_UP")
-            last_press = "KEY_UP"
+            vars.last_press = "KEY_UP"
             time.sleep(0.3)
 
         if not GPIO.input(KEY_RIGHT_PIN):
             print("KEY_RIGHT")
-            last_press = "KEY_RIGHT"
+            vars.last_press = "KEY_RIGHT"
             time.sleep(0.3)
 
         if not GPIO.input(KEY_DOWN_PIN):
             print("KEY_DOWN")
-            last_press = "KEY_DOWN"
+            vars.last_press = "KEY_DOWN"
             time.sleep(0.3)
 
         if not GPIO.input(KEY_LEFT_PIN):
             print("KEY_LEFT")
-            last_press = "KEY_LEFT"
+            vars.last_press = "KEY_LEFT"
             time.sleep(0.3)
 
         if not GPIO.input(KEY_PRESS_PIN):
             print("KEY_PRESS")
-            last_press = "KEY_PRESS"
+            vars.last_press = "KEY_PRESS"
             time.sleep(0.3)
 
         if not GPIO.input(KEY1_PIN):
             print("KEY1")
-            last_press = "KEY1"
+            vars.last_press = "KEY1"
             time.sleep(0.3)
 
         if not GPIO.input(KEY2_PIN):
             print("KEY2")
-            last_press = "KEY2"
+            vars.last_press = "KEY2"
             time.sleep(0.3)
 
         if not GPIO.input(KEY3_PIN):
             print("KEY3")
-            last_press = "KEY3"
+            vars.last_press = "KEY3"
             time.sleep(0.3)
