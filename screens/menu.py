8d64d2acc335c64a6000db05dc850d14d7aae35c
zp4rker <iam@zp4rker.com>
2023-01-18 11:41:41 +1100

    manual draw

--- screens/menu.py
+++ screens/menu.py
@@ -2,7 +2,9 @@ from PIL import Image, ImageDraw
 
 
 def show():
-    base = Image.open("screens/images/menu.png")
-    button = Image.open("screens/images/button-highlighted.png")
-    base.paste(button, (20, 20))
+    base = Image.new("RGB", (240, 240), "BLACK")
+    draw = ImageDraw.Draw(base)
+
+    draw.line([(0, 210), (240, 210)], fill="WHITE", width=1)
+
     return base
--- var.py
+++ var.py
@@ -1,5 +1,8 @@
+from PIL import ImageFont
+
 display = None
 cur_screen = None
+font = ImageFont.truetype("JetBrainsMono.ttf", size=16)
 
 last_press = None
 
