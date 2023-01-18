03fb19644ba841dbf0c492f12fe9412a4c36afc4
zp4rker <iam@zp4rker.com>
2023-01-18 22:06:11 +1100

    start adding stats to home screen

--- screens/home.py
+++ screens/home.py
@@ -1,9 +1,11 @@
+import socket
 import time
 from datetime import datetime
 
 from PIL import Image, ImageDraw
 
 import screens.menu
+import util
 import var
 
 
@@ -14,6 +16,11 @@ def show():
     draw.line([(0, 210), (240, 210)], fill="WHITE", width=1)
     timestr = time.strftime("%a %-d %b %Y | " + ("%H:%M" if var.blink else "%H %M")).format(datetime.now())
     draw.text((5, 215), text=timestr, font=var.font, fill="WHITE")
+
+    # stats
+    text = f"Hostname: {socket.gethostname()} ({socket.gethostbyname(socket.gethostname())})\n"
+
+    draw.multiline_text((10, 20), text=util.wrap_lines(text, var.font, 200), font=var.font, fill="WHITE")
 
     return base
 
