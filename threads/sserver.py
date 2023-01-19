73b7f0b8296ab9e275bcc7d3cca6fac364881d34
zp4rker <iam@zp4rker.com>
2023-01-19 14:52:32 +1100

    ignore timeouterrors

--- threads/sserver.py
+++ threads/sserver.py
@@ -11,20 +11,23 @@ def listen():
         sock.settimeout(0.5)
         sock.listen()
         while not var.quitting:
-            conn, addr = sock.accept()
-            with conn:
-                text = f"Received message from {addr}:\n"
+            try:
+                conn, addr = sock.accept()
+                with conn:
+                    text = f"Received message from {addr}:\n"
 
-                payload = ""
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    payload += data.decode()
-                text += payload
+                    payload = ""
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        payload += data.decode()
+                    text += payload
 
-                screens.message.message = text
-                var.cur_screen = screens.message.show
-                var.cur_handle = screens.message.handle
-                var.standby = False
-                var.last_active = datetime.now()
+                    screens.message.message = text
+                    var.cur_screen = screens.message.show
+                    var.cur_handle = screens.message.handle
+                    var.standby = False
+                    var.last_active = datetime.now()
+            except TimeoutError:
+                pass
