bff279e6ad2c9eef0c75f0fca5a7a915bbc1f845
zp4rker <iam@zp4rker.com>
2023-01-19 12:58:01 +1100

    trying something with the buttons in the menu

--- screens/menu.py
+++ screens/menu.py
@@ -26,14 +26,14 @@ def show():
     draw.text((120, 215), text=timestr, font=var.font, fill="WHITE", align="center", anchor="ma")
 
     for i in range(5):
-        y1 = 20 + (i * 35)
-        y2 = 50 + (i * 35)
+        y1 = 10 + (i * 45)
+        y2 = 50 + (i * 45)
 
         fill = "WHITE" if focus == i else "#353739"
         outline = "BLACK" if focus == i else "WHITE"
 
         draw.rectangle([20, y1, 220, y2], fill=fill, outline=outline)
-        draw.text((120, y1 + 4), text=buttons[i], font=var.font, fill=outline, align="center", anchor="ma")
+        draw.text((120, y1 + 9), text=buttons[i], font=var.font, fill=outline, align="center", anchor="ma")
 
     return base
 
