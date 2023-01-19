ec342e7059ddb0b9b0e47f5d59ee9b9f561bc8f0
zp4rker <iam@zp4rker.com>
2023-01-19 12:22:18 +1100

    center text in buttons

--- screens/shutdown.py
+++ screens/shutdown.py
@@ -31,7 +31,7 @@ def show():
         outline = "BLACK" if focus == i else "WHITE"
 
         draw.rectangle([20, y1, 220, y2], fill=fill, outline=outline)
-        draw.text((25, y1 + 5), text=buttons[i], font=var.font, fill=outline)
+        draw.text((120, y1 + 5), text=buttons[i], font=var.font, fill=outline, align="center", anchor="ma")
 
     return base
 
