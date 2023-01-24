3a52394abd78769f9401b16f5ea5c5af8eca0a41
zp4rker <iam@zp4rker.com>
2023-01-24 13:58:06 +1100

    handle time in the right way

--- screens/splash.py
+++ screens/splash.py
@@ -1,22 +1,25 @@
-import time
+import datetime
 
 from PIL import Image
 
 import screens.home
 from core import var
 
+last_frame = datetime.datetime.now()
+frame = 0
+
 
 def show():
+    global last_frame, frame
     base = Image.new("RGB", (240, 240), "#353739")
 
     with Image.open("assets/splash.gif") as im:
-        im.seek(0)
-        time.sleep(0.1)
         try:
-            while True:
-                im.seek(im.tell() + 1)
-                base.paste(im, (0, 0))
-                time.sleep(0.1)
+            if (datetime.datetime.now() - last_frame).microseconds >= 100:
+                frame += 1
+                last_frame = datetime.datetime.now()
+            im.seek(frame)
+            base.paste(im, (0, 0))
         except EOFError:
             var.cur_screen = screens.home.show
             var.cur_handle = screens.home.handle
