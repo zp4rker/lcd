18ca51acb9372346f0184fce5d15edfbbb15ccaa
zp4rker <iam@zp4rker.com>
2023-01-24 14:29:13 +1100

    only open image once?

--- screens/splash.py
+++ screens/splash.py
@@ -8,23 +8,24 @@ from core import var
 last_frame = datetime.datetime.now()
 frame = 0
 
+im = Image.open("assets/splash.gif")
+
 
 def show():
     global last_frame, frame
     base = Image.new("RGB", (240, 240), "#353739")
 
-    with Image.open("assets/splash.gif") as im:
-        try:
-            # if (datetime.datetime.now() - last_frame).microseconds >= 100:
-            #     print(f"frame {frame} - {(datetime.datetime.now() - last_frame).microseconds}ms")
-            #     frame += 1
-            #     last_frame = datetime.datetime.now()
-            im.seek(frame)
-            base.paste(im, (0, 0))
-            frame += 1
-        except EOFError:
-            var.cur_screen = screens.home.show
-            var.cur_handle = screens.home.handle
+    try:
+        # if (datetime.datetime.now() - last_frame).microseconds >= 100:
+        #     print(f"frame {frame} - {(datetime.datetime.now() - last_frame).microseconds}ms")
+        #     frame += 1
+        #     last_frame = datetime.datetime.now()
+        im.seek(frame)
+        base.paste(im, (0, 0))
+        frame += 1
+    except EOFError:
+        var.cur_screen = screens.home.show
+        var.cur_handle = screens.home.handle
 
     return base
 
